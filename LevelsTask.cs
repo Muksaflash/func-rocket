8a0b8640e4cdf67bec78bde59ce5b1ceb59a2044
Александр <muksaflash@gmail.com>
2022-08-17 20:17:42 +0300

    Сделал всё без рефакторинга

--- LevelsTask.cs
+++ LevelsTask.cs
@@ -10,7 +10,7 @@ namespace func_rocket
 		public static IEnumerable<Level> CreateLevels()
 		{
 			yield return new Level("Zero", 
-				new Rocket(new Vector(200, 500), Vector.Zero, -0.6 * Math.PI),
+				new Rocket(new Vector(200, 500), Vector.Zero, -0.5 * Math.PI),
 				new Vector(600, 200), 
 				(size, v) => Vector.Zero, standardPhysics);
 			yield return new Level("Heavy",
@@ -32,7 +32,21 @@ namespace func_rocket
             yield return new Level("BlackHole",
                 new Rocket(defaultRRocketLocation, Vector.Zero, -0.5 * Math.PI),
                 target,
-                (size, v) => {var anomalyPoints = (defaultRRocketLocation - target); var n = Math.Atan2(-v.X+anomalyPoints.X, -v.Y+anomalyPoints.Y); return new Vector(300 * (anomalyPoints - v).Length / Math.Pow((anomalyPoints - v).Length + 1, 2)*Math.Sin(n), 300 * (anomalyPoints - v).Length / Math.Pow((anomalyPoints - v).Length + 1,2)*Math.Cos(n));}, standardPhysics);
+                (size, v) => {var anomalyPoint = new Vector((defaultRRocketLocation.X + target.X)/2, (defaultRRocketLocation.Y+target.Y)/2); var n = Math.Atan2(-v.X+anomalyPoint.X, -v.Y+anomalyPoint.Y); return new Vector(300 * (anomalyPoint - v).Length / Math.Pow((anomalyPoint - v).Length + 1, 2)*Math.Sin(n), 300 * (anomalyPoint - v).Length / Math.Pow((anomalyPoint - v).Length + 1,2)*Math.Cos(n));}, standardPhysics);
+
+            yield return new Level("BlackAndWhite",
+                new Rocket(defaultRRocketLocation, Vector.Zero, -0.5 * Math.PI),
+                target,
+                (size, v) => 
+                {
+                    var anomalyPoint = new Vector((defaultRRocketLocation.X + target.X)/2, (defaultRRocketLocation.Y+target.Y)/2); 
+                    var n = Math.Atan2(-v.X+anomalyPoint.X, -v.Y+anomalyPoint.Y); 
+                    var m = Math.Atan2(v.X-target.X, v.Y-target.Y);
+                    return new Vector(((300 * (anomalyPoint - v).Length / Math.Pow((anomalyPoint - v).Length + 1, 2)*Math.Sin(n))+(140 * (target - v).Length / Math.Pow((target - v).Length + 1, 2)*Math.Sin(m)))/2, 
+                        ((300 * (anomalyPoint - v).Length / Math.Pow((anomalyPoint - v).Length + 1,2)*Math.Cos(n))+(140 * (target - v).Length / Math.Pow((target - v).Length + 1, 2)*Math.Cos(m)))/2);
+                }, 
+                standardPhysics);
+        
         }
 	}
 }
