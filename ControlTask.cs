231aa16203ae30942af894d794ae2c07ff924a2e
Александр <muksaflash@gmail.com>
2022-08-19 15:43:51 +0300

    С последней задачей

--- ControlTask.cs
+++ ControlTask.cs
@@ -4,9 +4,19 @@ namespace func_rocket
 {
 	public class ControlTask
 	{
-		public static Turn ControlRocket(Rocket rocket, Vector target)
+        private static double VectorsAngle(Vector v, Vector defaultTarget, bool isFromPointGravity = true)
+        {
+            var factor = 1;
+            if (!isFromPointGravity) factor = -1;
+            return Math.Atan2(factor * (v.X - defaultTarget.X), factor * (v.Y - defaultTarget.Y));
+        }
+        public static Turn ControlRocket(Rocket rocket, Vector target)
 		{
-			return Turn.None;
-		}
+            var targetDirection = target - rocket.Location;
+            var targetAngle = Math.Atan2(targetDirection.Y, targetDirection.X);
+            if ((2 * rocket.Direction + 4 * rocket.Velocity.Angle) / 6 <= targetAngle)
+                return Turn.Right;
+            return Turn.Left;
+        }
 	}
 }
